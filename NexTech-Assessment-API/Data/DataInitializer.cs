55556b28a12da4505616875a2b0330fe2f6a06fd
Phillip Sylvain <psylvain324@gmail.com>
2020-09-08 22:18:08 -0400

    Minor update.
    In memory data for test purposes. Just getting both Git projects up to date.

--- NexTech-Assessment-API/Data/DataInitializer.cs
+++ NexTech-Assessment-API/Data/DataInitializer.cs
@@ -1,11 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NexTech_Assessment_API.Data;
 using TechAssessment.Models;
 
 namespace NexTech_Assessment_NUnit.Data
 {
     public class DataInitializer
     {
+        public static void Initialize(DatabaseContext databaseContext)
+        {
+            databaseContext.Database.EnsureCreated();
+            if (databaseContext.Stories.Any())
+            {
+                return;
+            }
+        }
+
         public static List<Story> GetAllStories()
         {
             var stories = new List<Story>
@@ -13,27 +23,62 @@ namespace NexTech_Assessment_NUnit.Data
                 new Story()
                 {
                     Url = "www.test234.com",
-                    Title = "123 - Test1"
-                },  
+                    Title = "123 - Test1",
+                    By = "Phillip Sylvain",
+                    Id = 12345,
+                    Descendants = 0,
+                    Kids = {},
+                    Score = 100,
+                    Time = "01051994",
+                    Type = "Test"
+                },
                 new Story()
                 {
                     Url = "www.test234.com",
-                    Title = "123 - Test2"
-                },  
+                    Title = "123 - Test2",
+                    By = "Phillip Sylvain",
+                    Id = 12345,
+                    Descendants = 0,
+                    Kids = {},
+                    Score = 100,
+                    Time = "01051994",
+                    Type = "Test"
+                },
                 new Story()
                 {
                     Url = "www.test345.com",
-                    Title = "123 - Test3"
-                },  
+                    Title = "123 - Test3",
+                    By = "Phillip Sylvain",
+                    Id = 12345,
+                    Descendants = 0,
+                    Kids = {},
+                    Score = 100,
+                    Time = "01051994",
+                    Type = "Test"
+                },
                 new Story()
                 {
                     Url = "www.test456.com",
-                    Title = "123 - Test4"
-                },  
+                    Title = "123 - Test4",
+                    By = "Phillip Sylvain",
+                    Id = 12345,
+                    Descendants = 0,
+                    Kids = {},
+                    Score = 100,
+                    Time = "01051994",
+                    Type = "Test"
+                },
                 new Story()
                 {
                     Url = "www.test567.com",
-                    Title = "Learning - Test5"
+                    Title = "Learning - Test5",
+                    By = "Phillip Sylvain",
+                    Id = 12345,
+                    Descendants = 0,
+                    Kids = {},
+                    Score = 100,
+                    Time = "01051994",
+                    Type = "Test"
                 }
             };
 
